dab9d9c7db8d8d463400b4b67a766f62c8043013
drafffffff <i0.0ia@qq.com>
2023-07-23 02:19:46 +0800

    7.23

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import staticAdapter from "solid-start-static";
+// import staticAdapter from "solid-start-static";
 
 export default defineConfig({
-  plugins: [solid({ ssr: true, adapter: staticAdapter() })],
+  plugins: [solid({ ssr: true })],
+  // plugins: [solid({ ssr: true, adapter: staticAdapter() })],
+  // base: "/zstuid-soild/",
 });
